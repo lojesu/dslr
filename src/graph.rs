cb8ddf5dc9f6d8800e38fed35e33a32c7ccff74a
lojesu <lojesu@student.42.fr>
2022-07-04 18:20:53 +0200

    graph part(histogram) end

--- src/graph.rs
+++ src/graph.rs
@@ -1,8 +1,72 @@
 use plotters::prelude::*;
 use crate::feature::Feature;
 
+pub fn graph(feature: Feature, all_std: Vec<f32>) {
+    let root = BitMapBackend::new("multi-panel-figure.png", (640, 480)).into_drawing_area();
+        root.fill(&WHITE).unwrap();
+    let (upper, lower) = root.split_vertically((70).percent());
+
+    let title = format!("values for {} feature", feature.get_name());
+    let mut lower_chart = ChartBuilder::on(&lower)
+        .set_label_area_size(LabelAreaPosition::Left, 30)
+        .set_label_area_size(LabelAreaPosition::Right, 30)
+        .set_label_area_size(LabelAreaPosition::Bottom, 30)
+        .caption(&title, ("sans-serif", 20))
+        .build_cartesian_2d(1..feature.get_count(), (feature.get_min().unwrap() as i32)..(feature.get_max().unwrap() as i32))
+        .unwrap();
+
+    lower_chart.configure_mesh().draw().unwrap();
+
+    let new_values: Result<Vec<f32>, _> = feature.get_values().iter().map(|x| x.parse()).collect();
+
+    match new_values {
+        Ok(mut data) => {
+            data.sort_by(|a, b| {
+                match a.partial_cmp(b) {
+                    Some(ord) => ord,
+                    _ => {
+                        println!("unexpected error when sort values");
+                        unreachable!();
+                    }
+                }
+            });
+            lower_chart.draw_series((0..).zip(data.iter()).map(|(x, y)| {
+                let mut bar = Rectangle::new([(x, 0), (x, *y as i32)], RED.filled());
+                bar.set_margin(0, 0, 2, 2);
+                bar
+            }))
+            .unwrap();
+            },
+        _ => ()
+    }
+    
+    let mut all_std_max = all_std.get(0).unwrap();
+    all_std.iter().for_each(|x| {
+        if x > all_std_max {
+            all_std_max = x;
+        }
+    });
+    let mut upper_chart = ChartBuilder::on(&upper)
+        .set_label_area_size(LabelAreaPosition::Left, 30)
+        .set_label_area_size(LabelAreaPosition::Right, 30)
+        .set_label_area_size(LabelAreaPosition::Bottom, 30)
+        .caption("all Std value", ("sans-serif", 20))
+        .build_cartesian_2d(1..(all_std.len() - 1), 0..100)
+        .unwrap();
+    upper_chart.configure_mesh().draw().unwrap();
+    
+    upper_chart.draw_series((0..).zip(all_std.iter()).map(|(x, y)| {
+        let mut bar = Rectangle::new([(x, 0), (x, *y as i32)], RED.filled());
+        bar.set_margin(0, 0, 5, 5);
+        bar
+    }))
+    .unwrap();
+}
+
+/*
 pub fn graph(feature: Feature) {
-    let root_area = BitMapBackend::new("histogram.png", (900, 400))
+    let title = format!("{}.png", feature.get_name());
+    let root_area = BitMapBackend::new(&title, (900, 400))
     .into_drawing_area();
     root_area.fill(&WHITE).unwrap();
 
@@ -39,3 +103,4 @@ pub fn graph(feature: Feature) {
         _ => ()
     }
 }
+*/
--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,24 @@ use graph::graph;
 
 fn main() {
     let features = describe();
-    graph(features[11].clone());
+    let mut feature_std_min = features.get(0).unwrap();
+    let mut all_std: Vec<f32> = Vec::new();
+    features.iter().for_each(|x| {
+        match x.get_std() {
+            Some(x_std) => {
+                all_std.push(x_std);
+                match feature_std_min.get_std() {
+                    Some(stdmin_std) => {
+                        if x_std < stdmin_std {
+                            feature_std_min = x;
+                        }
+                    },
+                    _ => ()
+                }
+            },
+            _ => ()
+        }
+    });
+    //graph(feature_std_min.clone(), all_std);
+    graph(features[8].clone(), all_std);
 }
